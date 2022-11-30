10a482f7065b8e95e8b6d92b6fbc0360c296e12a
nikosajadi <na.sajadii.29@gmail.com>
2022-12-01 00:49:24 +0100

    updateSavedColumns

--- script.js
+++ script.js
@@ -53,8 +53,8 @@ function getSavedColumns() {
 }
 }
 //for test we call this function
-getSavedColumns();
-updateSavedColumns();
+// getSavedColumns();
+// updateSavedColumns();
 
 //set the value here and saving this value into localStorage
 //Set the value
@@ -75,25 +75,27 @@ function createItemEl(columnEl, column, item, index) {
   console.log('column:', column);
   console.log('item:', item);
   console.log('index:', index);
-  //List Item
-  //creates an element,a list element specificaly adding in the class of drag item so we creating a lis element.
+      //List Item
+      //creates an element,a list element specificaly adding in the class of drag item so we creating a lis element.
   const listEl = document.createElement('li');
-  //after run every item in our array, here we adding our css class of drag item
+      //after run every item in our array, here we adding our css class of drag item
   listEl.classList.add('drag-item');
-  // the next thing that we add to the text item
+      // the next thing that we add to the text item
   listEl.textContent = item;
-  //Make an Element Draggable
+     //Make an Element Draggable
   listEl.draggable = true;
-  listEl.setAttribute('ondragstrart', 'drag(event');
-  // we going to Append child and use list element
-  //next step Append 
-  columnEl.appendChild(listEl);
+  listEl.setAttribute('ondragstart', 'drag(event)');
+  listEl.contentEditable = true;
+     // we going to Append child and use list element
+     //next step Append 
+     columnEl.appendChild(listEl);
 }
 
   // Update Columns in DOM - Reset HTML, Filter Array, Update localStorage
 function updateDOM() {
   // Check localStorage once
   if (!updatedOnLoad) {
+      //we only wanted to run,this gets saved comb's function once when we load the page.
     getSavedColumns();
   }
   //this will allow us reset and remove all of the element within our  list
@@ -108,20 +110,24 @@ function updateDOM() {
   // Progress Column
   progressListEl.textContent = '';
   progressListArray.forEach((progressItem, index) => {
-    createItemEl(progressListEl, 1, progressItem, index);
+    createItemEl(progressListEl, 0, progressItem, index);
   });
   //progressListArray = filterArray(progressListArray);
   // Complete Column
   completeListEl.textContent = '';
   completeListArray.forEach((completeItem, index) => {
-    createItemEl(completeListEl, 2, completeItem, index);
+    createItemEl(completeListEl, 0, completeItem, index);
   });
   //completeListArray = filterArray(completeListArray);
   // On Hold Column
   onHoldListEl.textContent = '';
   onHoldListArray.forEach((onHoldItem, index) => {
-    createItemEl(onHoldListEl, 3, onHoldItem, index);
+    createItemEl(onHoldListEl, 0, onHoldItem, index);
   }); 
+
+  // Don't run more than once, Update Local Storage
+  updatedOnLoad = true;
+  updateSavedColumns();
 }
 
 //Allows arrays to reflect Drag and Drop items
@@ -143,10 +149,18 @@ onHoldListArray = [];
 for (let i = 0; i < onHoldListEl.children.length; i++) {
   onHoldListArray.push(onHoldListEl.children[i].textContent);
 }
+  // acually rebuild everything
 updateDOM();
 }
 
- 
+ //When Item Enters Column Area
+function dragEnter(column) {
+  // console.log(listColumns[column]);
+   //we want to use our class lists,not to add the Css class also add padding and add the colors
+   listColumns[column].classList.add('over');
+   // for show us dragEnter(number)
+   currentColumn= column;
+ }
  
 
 
@@ -155,38 +169,35 @@ updateDOM();
 function drag(e){
   draggedItem = e.target;
   console.log('draggedItem:' , draggedItem);
+  dragging = true;
 }
 
 // by defult, data cannot be dropped in other element.to allow a drop,we must prevent the defult handling of element,this method helep us to drop the item that we draggedinto
 //Column allows for Item to Drop
 function  allowDrop(e) {
-  e.preventDeault();
-}
-
-//When Item Enters Column Area
-function dragEnter(column) {
- // console.log(listColumns[column]);
-  //we want to use our class lists,not to add the Css class also add padding and add the colors
-  listColumns[column].classList.add('over');
-  // for show us dragEnter(number)
-  currentColumn= column;
+  e.preventDefault();
 }
 
 
-//Dropping Item in column
+
+
+// Dropping Item in Column
 function drop(e) {
-  e.preventDefult();
-  //Remove Background Color/Padding 
+  e.preventDefault();
+  const parent = listColumns[currentColumn];
+  // Remove Background Color/Padding
   listColumns.forEach((column) => {
     column.classList.remove('over');
   });
-  // Add Item to Column (creat parent here)
-  const parent = listColumns[currentColumn];
+  // Add item to Column
   parent.appendChild(draggedItem);
-  rebuildArrays()
+  // Dragging complete
+  dragging = false;
+  rebuildArrays();
 }
-//on load
+
+// On Load
 updateDOM();
 
-//update our array and update our local storage
+
 
